1fb6cf8e893a70dd0ff9b3a70c1c5d6da9ea5eab
Mr-Vegit <85156427+Mr-Vegit@users.noreply.github.com>
2024-12-15 10:48:17 +0530

    .

--- cacheSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const CacheSchema = new mongoose.Schema({
-    animeData:Array,
-    filter:Array,
-});
-// UserSchema.plugin(uniqueValidator)
-const Caches = mongoose.model("Caches", CacheSchema);
-module.exports = Caches;
--- static/js/Signin.js
+++
@@ -1,7 +0,0 @@
-const Alert = document.querySelector(".alert")
-if (Alert) {
-  let i=5;
-  setTimeout(() => {
-    Alert.style.display = "none";
-  }, 6000);
-}
